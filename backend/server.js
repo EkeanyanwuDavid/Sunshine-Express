494ba099026f8b6fbac419c7d1687e49eac18876
David <ekeanyanwudavid@gmail.com>
2026-04-14 03:32:27 +0100

    inital rest api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
 
