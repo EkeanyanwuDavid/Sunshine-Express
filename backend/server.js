e79d2c0a4b49c852d86af6ede3d325a18579b145
David <ekeanyanwudavid@gmail.com>
2026-06-17 05:02:02 +0100

    add axios to frontend dependencies

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
-const path = require("path");
 const cors = require("cors");
 
 const paymentRoutes = require("./routes/paymentRoutes");
@@ -17,7 +16,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://sunshineshoppingexpress.netlify.app",
+    ],
     credentials: true,
   }),
 );
@@ -29,18 +31,5 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/payments", paymentRoutes);
 app.use("/api/orders", orderRoutes);
 
-// Serve frontend static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html"),
-    );
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("please set to production...");
-  });
-}
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
